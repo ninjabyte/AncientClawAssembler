713c654b9d2f4692e786086a15f5a240c43cf8de
Benedikt Müssig <benedikt.muessig@gmail.com>
2015-08-23 16:25:10 +0200

    Some fixes and improvements
    - Changed assembly name
    - Fixed assembly info
    - Reworked command line
    - Removed FSharp reference

--- ClawAssembler/ConsoleOptions.cs
+++ ClawAssembler/ConsoleOptions.cs
@@ -6,19 +6,19 @@ namespace ClawAssembler
 {
 	public class ConsoleOptions
 	{
+		[Value(0, MetaName = "Input Filename", MetaValue = "STRING", HelpText = "Input file path", Required = true)]
+		public string InputFilename{ get; set; }
+
+		[Option('o', "output", Default = "out.cex",
+			HelpText = "Path to where to output the executable.")]
+		public string OutputFilename { get; set; }
+
 		[Option('v', "verbose", Default = false,
 			HelpText = "Prints all messages to standard output.")]
 		public bool Verbose { get; set; }
 
 		[Option('l', "errorlevel", Default = ErrorLevel.Error)]
 		public ErrorLevel ErrorLevel { get; set; }
-
-		[Value(0, HelpText = "Input file path", Required = true)]
-		public string InputFilename{ get; set; }
-
-		[Option('o', "output", Default = "out.cex",
-			HelpText = "Path to where to output the executable.")]
-		public string OutputFilename { get; set; }
 	}
 }
 
